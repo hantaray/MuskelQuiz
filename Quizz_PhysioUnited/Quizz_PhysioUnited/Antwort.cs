05a3f337edb8f79509ba1eb1b5f71bad5264d57b
hasashi <hasashi@e.mail.de>
2020-07-09 22:04:53 +0200

    files ausgetauscht

---
+++ Quizz_PhysioUnited/Quizz_PhysioUnited/Antwort.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizz_PhysioUnited
+{
+    public class Antwort
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int GruppeID { get; set; }
+        public string AntwortText { get; set; }
+    }
+}
---
+++ Quizz_PhysioUnited/Quizz_PhysioUnited/Datenbank.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Quizz_PhysioUnited
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Frage>().Wait();
+            _database.CreateTableAsync<Antwort>().Wait();
+        }
+
+        //public Task<List<Frage>> GetFrageAsync()
+        //{
+        //    return _database.Table<Frage>().Where(id="1").ToListAsync();
+        //}
+        public Task<List<Antwort>> GetAntwortAsync()
+        {
+            return _database.Table<Antwort>().ToListAsync();
+        }
+
+        public Task<int> SaveFrageAsync(Frage frage)
+        {
+            return _database.InsertAsync(frage);
+        }
+
+        public Task<int> SaveAntwortAsync(Antwort antwort)
+        {
+            return _database.InsertAsync(antwort);
+        }
+    }
+}
