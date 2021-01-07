31b1055100a6fffdb320582bbd57d6900eac3ecb
Konne KLUK <konne@kombinat-luk.de>
2021-01-07 14:03:09 +0100

    buttons are reponsive and work in all tested resoltuions

--- Quizz_PhysioUnited/Quizz_PhysioUnited/LoadingPage.xaml.cs
+++ Quizz_PhysioUnited/Quizz_PhysioUnited/LoadingPage.xaml.cs
@@ -19,6 +19,8 @@ namespace Quizz_PhysioUnited
             LoadData();            
         }
 
+        //try to start LoadData with OnAppearing
+
         private async void LoadData()
         {
             if (InternetTester.TestConnection())
